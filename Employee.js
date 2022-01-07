bbdd2f2c981539330a1e75c8a0e4aac9aafa7882
vsinky <sinkykumariverma@gmail.com>
2022-01-07 22:30:12 +0530

    Added part time wage

--- Employee.js
+++ Employee.js
@@ -1,9 +1,22 @@
-const is_present = 1;
+const IS_PARTTIME = 1;
+const IS_FULLTIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-var employeePresent = Math.floor((Math.random() * 10) % 2);
-console.log(employeePresent);
-if (employeePresent == is_present) {
-    console.log("employee is present");
-} else {
-    console.log("the employee is absent");
+let emphours = 0;
+empcheck = (Math.floor(Math.random() * 10) % 3);
+switch (empcheck) {
+    case IS_PARTTIME:
+        emphours = PART_TIME_HOURS;
+        break;
+    case IS_FULLTIME:
+        emphours = FULL_TIME_HOURS;
+        break;
+    default:
+        emphours = 0;
+        break;
 }
+
+let empWage = WAGE_PER_HOUR * emphours;
+console.log("the employee wage is " + empWage);
